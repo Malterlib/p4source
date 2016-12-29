0e2c10f3157c62650bff3a876b022062ba17d93f
Erik Olofsson <erik.olofsson@hansoft.com>
2016-12-29 23:21:26 +0000

    Fix c pch error

--- sys/stdhdrs.h
+++ sys/stdhdrs.h
@@ -22,6 +22,7 @@
  */
 
 #pragma once
+#ifdef __cplusplus
 
 # ifdef OS_VMS
 # define _POSIX_EXIT  // to get exit status right from stdlib.h
@@ -944,4 +945,4 @@ typedef int FD_PTR;
 #  endif
 # endif
 
-# endif // P4STDHDRS_H
+#endif // __cplusplus
