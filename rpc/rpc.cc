2b21f435570c540c46f08405068ebb7b87bd561f
Erik Olofsson <erik.olofsson@hansoft.com>
2021-01-10 12:16:08 +0100

    Check transport pointer before use it

--- rpc/rpc.cc
+++ rpc/rpc.cc
@@ -1099,6 +1099,9 @@ Rpc::DispatchOne( RpcDispatcher *dispatcher, bool passError )
 
 	// Receive sender's buffer and then parse the variables out.
 	
+	if (!transport)
+		return;
+
 	timer->Start();
 
 	int delay = p4tunable.Get( P4TUNE_RPC_DELAY );
